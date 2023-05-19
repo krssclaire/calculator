0e57ac097ce24e46b8a08b96849798d39af15313
Kristel <kristelclaire.02@gmail.com>
2023-05-19 14:34:06 +0100

    Allow the user to work multiple digits numbers
    The user can now input and execute calculations with multiple digits
    numbers and delete the last digit if the user has mistaken.
    Plus, the user can input float numbers.
    
    Note:   Still need to improve calculations bugs:
            - add functionality to '+/-' button;
            - the first operator value stored will remain the same even
              though the second operator changes.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const numbers = document.querySelectorAll('.numbers');
 const operators = document.querySelectorAll('.operators');
-const equal = document.querySelectorAll('.equal');
-const percentage = document.querySelector('.percentage');
+const equal = document.querySelector('.equal');
+const percentage = document.querySelectorAll('.percentage');
 const display = document.querySelector('.text');
 const result = document.querySelector('.subtext');
 const clearAll = document.querySelectorAll('.clear-all');
+const cancel = document.querySelectorAll('.canc');
 
-
+let arr = [];
 let n;
 let n1;
 let n2;
@@ -14,32 +15,52 @@ let n2;
 
 // Display numbers 
 numbers.forEach(number => {
-    number.addEventListener('click', displayNumbers);
+    number.addEventListener('click', (e) => {
+        function displayNumbers() {
+            let value =  e.target.textContent;
+            arr.push(value);
+            n = arr.join('');
+            display.textContent = n; 
+            console.log(n);
+            
+            cancel.forEach(canc => {
+                canc.addEventListener('click', (e) => {
+                    function deleteDigit() {
+                        let corrected;
+                        arr = n.split('');
+                        arr.pop();
+                        corrected = arr.join('');
+                        console.log(corrected);
+                        display.textContent = corrected;
+                    }
+                    deleteDigit();
+                });
+            });
+        }
+        displayNumbers();
+    });
 });
 
-function displayNumbers(e) {
-    n = e.target.textContent;
-    display.textContent = n;
-    /*
-    let text = document.createTextNode(e.target.textContent);
-    n = display.appendChild(text); */
-    console.log(n);
-}
-
-/* Operate */
+// Operate 
 operators.forEach(operator => {
     operator.addEventListener('click', (e) => {
+        display.textContent = ' ';
+        arr = [];
         operator = e.target.textContent;
         console.log(operator);
-
-        equal.forEach(eq => {
-            eq.addEventListener('click', (e) => {
-                n2 = parseInt(n, 10);
-                console.log(n2);
-                operate(operator, n1, n2)
+        
+        percentage.forEach(percent => {
+            percent.addEventListener('click', (e) => {
+                n2 = parseFloat(n) / 100;
+                result.textContent = n2;
             });
         });
-        n1 = parseInt(n, 10);
+
+        equal.addEventListener('click', (e) => {
+                n2 = parseFloat(n);
+                operate(operator, n1, n2)
+        });
+        n1 = parseFloat(n, 10);
     });
 });
 
@@ -57,6 +78,8 @@ const multiply = (n1, n2) => {
     console.log(n1*n2);
 }
 const divide = (n1, n2) => {
+    if (n2 == 0) result.textContent = 'ERROR';
+    console.log('ERROR');
     result.textContent = n1 / n2;
     console.log(n1/n2);
 }
@@ -78,10 +101,14 @@ function operate(operator, n1, n2) {
         }
 }
 
-/* Clean display */
+/* Clean display and reset values */
 clearAll.forEach(clear => {
     clear.addEventListener('click', (e) => {
         display.textContent = ' ';
         result.textContent = ' ';
+        operator = ' ';
+        arr = [];
+        n1 = 0;
+        n2 = 0;
     });
 });
