addc81500f2bd53999f730b097974091955af37c
Kristel <kristelclaire.02@gmail.com>
2023-05-25 21:40:17 +0100

    Add more functions
    In particular there is now a percentage function
    
    - percentage        To-do
    - dot               Done
    - cancel all        Done
    - operators anomaly Done (new anomaly*)
    - positive/negative To-do
    
    *new bug concerning operators button
     when the operators are pressed before entering all of the numbers or an
     operator it still returns NaN values

--- script.js
+++ script.js
@@ -5,26 +5,26 @@ const pointButton = document.querySelector('.point');
 const equalButton = document.querySelector('.equal');
 const clearAllButton = document.querySelector('.clear-all');
 const cancelButton = document.querySelector('.canc');
+const signButton = document.querySelector('.sign');
+const percentageButton = document.querySelector('.percentage');
 
 const operationDisplay = document.querySelector('.operation');
-const resultDisplay = document.querySelector('.result'); // input / output
+const resultDisplay = document.querySelector('.result');
 
 let n1 = '';
 let n2 = '';
 let currentOperator = null;
 let displayValue = '';
-
+let negative = false;
+let negativeVal;
 
 equalButton.addEventListener('click', calculate);
 clearAllButton.addEventListener('click', clearAll);
 pointButton.addEventListener('click', appendPoint);
 cancelButton.addEventListener('click', deleteDigit);
+percentageButton.addEventListener('click', calculatePercentage);
+signButton.addEventListener('click', changeSign);
 
-buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        if (resultDisplay.textContent === 'ERROR') clearAll();
-    });
-});
 numberButtons.forEach(btn => {
     btn.addEventListener('click', () => appendNumber(btn.textContent));
 });
@@ -32,17 +32,14 @@ operatorButtons.forEach(btn => {
     btn.addEventListener('click', () => setOperation(btn.textContent));
 });
 
-/*
-function resetResultDisplay() {
-    resultDisplay.textContent = '';
-}*/
-
 function appendNumber(num) {
+    if (resultDisplay.textContent === 'ERROR') clearAll();
     displayValue += num;
     resultDisplay.textContent = displayValue;
 }
 
 function appendPoint() {
+    if (resultDisplay.textContent === 'ERROR') clearAll();
     if (resultDisplay.textContent === '0' || resultDisplay.textContent === '' ) {
         displayValue = '0';
     }
@@ -54,13 +51,29 @@ function appendPoint() {
     }
 }
 
+function changeSign() {
+    if (resultDisplay.textContent === 'ERROR') clearAll();
+    if (negative) {
+        resultDisplay.textContent = displayValue;
+        negative = false;
+    } else  {
+        negativeVal = `-${displayValue}`;
+        
+        resultDisplay.textContent = negativeVal;
+        negative = true;
+    }
+}
+
 function deleteDigit() {
+    if (resultDisplay.textContent === 'ERROR') clearAll();
     displayValue = displayValue.toString().slice(0, -1);
     resultDisplay.textContent = displayValue;
 }
 
 function setOperation(operator) {
+    if (resultDisplay.textContent === 'ERROR') clearAll();
     operationDisplay.textContent = '';
+    if (resultDisplay.textContent.includes('-')) displayValue = negativeVal;
     if (currentOperator !== null) {
         calculate();
         currentOperator = operator;
@@ -71,10 +84,11 @@ function setOperation(operator) {
         currentOperator = operator;
         operationDisplay.textContent += `${n1} ${currentOperator} `;
     }
-    displayValue = ''
+    displayValue = '';
 }
 
 function calculate() {
+    if (resultDisplay.textContent === 'ERROR') clearAll();
     n2 = displayValue;
     operationDisplay.textContent = `${n1} ${currentOperator} ${n2} =`;
     if (currentOperator === ':' && n2 === '0') {
@@ -87,13 +101,27 @@ function calculate() {
     }
 }
 
+function calculatePercentage() {
+    if (resultDisplay.textContent === 'ERROR') clearAll();
+    if (operationDisplay.textContent === '') {
+        displayValue = n1 / 100;
+        resultDisplay.textContent =  displayValue;
+    }
+    if (currentOperator !== null) {
+        n2 = displayValue
+        let percent = n1 * n2 / 100;
+        resultDisplay.textContent = roundResult(operate(currentOperator, n1, percent));
+        displayValue = resultDisplay.textContent;
+    } 
+}
+
 function clearAll() {
     displayValue = '';
     currentOperator = null;
     n1 = '';
     n2 = '';
     operationDisplay.textContent = '';
-    resultDisplay.textContent = '';
+    resultDisplay.textContent = '0';
 }
 
 const roundResult = num => Math.round(num * 1000) / 1000;
@@ -140,4 +168,5 @@ function useKeyboardInput(e) {
     if (e.key === 'Escape') clearAll();
     if (e.key === '.') appendPoint();
     if (e.key === 'Backspace') deleteDigit();
+    if (e.key === '%') calculatePercentage();
 }
