0434d2b16e6504e0d661d15cbc357989c2558654
Kristel <kristelclaire.02@gmail.com>
2023-05-25 15:56:01 +0100

    Improve JS by resolving some bugs of issue #002
    Add functions on click and on keydown events.
    
    The calculator can now execute calculations by storing the result of the
    first operation as the first operand and then executing a new calculation
    when an operator is chosen.
    
    Recap:
    - percentage        To-do
    - dot               Done
    - cancel all        Done
    - operators anomaly Done (new anomaly*)
    - positive/negative To-do
    
    *new bug concerning operators button
     when the operators are pressed before entering all of the numbers or an
     operator it still returns NaN values

--- script.js
+++ script.js
@@ -1,45 +1,102 @@
+const buttons = document.querySelectorAll('.grid div');
 const numberButtons = document.querySelectorAll('.numbers');
 const operatorButtons = document.querySelectorAll('.operators');
+const pointButton = document.querySelector('.point');
 const equalButton = document.querySelector('.equal');
+const clearAllButton = document.querySelector('.clear-all');
+const cancelButton = document.querySelector('.canc');
 
 const operationDisplay = document.querySelector('.operation');
 const resultDisplay = document.querySelector('.result'); // input / output
 
 let n1 = '';
 let n2 = '';
-let currentOperator = '';
+let currentOperator = null;
 let displayValue = '';
 
-equalButton.addEventListener('click', calculate);
 
+equalButton.addEventListener('click', calculate);
+clearAllButton.addEventListener('click', clearAll);
+pointButton.addEventListener('click', appendPoint);
+cancelButton.addEventListener('click', deleteDigit);
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (resultDisplay.textContent === 'ERROR') clearAll();
+    });
+});
 numberButtons.forEach(btn => {
     btn.addEventListener('click', () => appendNumber(btn.textContent));
 });
 operatorButtons.forEach(btn => {
     btn.addEventListener('click', () => setOperation(btn.textContent));
-})
+});
+
+/*
+function resetResultDisplay() {
+    resultDisplay.textContent = '';
+}*/
 
 function appendNumber(num) {
     displayValue += num;
     resultDisplay.textContent = displayValue;
-    //operationDisplay.textContent += displayValue;
+}
+
+function appendPoint() {
+    if (resultDisplay.textContent === '0' || resultDisplay.textContent === '' ) {
+        displayValue = '0';
+    }
+    if (!resultDisplay.textContent.includes('.')) {
+        displayValue += '.';
+        resultDisplay.textContent = displayValue;
+    } else {
+        return null;
+    }
+}
+
+function deleteDigit() {
+    displayValue = displayValue.toString().slice(0, -1);
+    resultDisplay.textContent = displayValue;
 }
 
 function setOperation(operator) {
-    n1 = displayValue;
-    currentOperator = operator;
-    operationDisplay.textContent += `${n1} ${currentOperator} `;
-    displayValue = '';
-
-    //if (currentOperator === '=') operate();
-    
+    operationDisplay.textContent = '';
+    if (currentOperator !== null) {
+        calculate();
+        currentOperator = operator;
+        displayValue = resultDisplay.textContent;
+        operationDisplay.textContent = `${displayValue} ${currentOperator}` ;
+    } else {
+        n1 = displayValue;
+        currentOperator = operator;
+        operationDisplay.textContent += `${n1} ${currentOperator} `;
+    }
+    displayValue = ''
 }
 
 function calculate() {
     n2 = displayValue;
-    operationDisplay.textContent += `${n2} =`
-    resultDisplay.textContent = operate(currentOperator, n1, n2);
+    operationDisplay.textContent = `${n1} ${currentOperator} ${n2} =`;
+    if (currentOperator === ':' && n2 === '0') {
+        resultDisplay.textContent = 'ERROR';
+    } else {
+        resultDisplay.textContent = roundResult(operate(currentOperator, n1, n2));
+        displayValue = resultDisplay.textContent;
+        n1 = displayValue;
+        currentOperator = null;
+    }
 }
+
+function clearAll() {
+    displayValue = '';
+    currentOperator = null;
+    n1 = '';
+    n2 = '';
+    operationDisplay.textContent = '';
+    resultDisplay.textContent = '';
+}
+
+const roundResult = num => Math.round(num * 1000) / 1000;
 
 // Operate
 const add = (a, b) => a + b;
@@ -52,7 +109,11 @@ function operate(operator, a, b) {
     b = Number(b);
     switch (operator) {
         case ':':
-            return divide(a, b);
+            if (b === 0) {
+                return null;
+            } else {
+                return divide(a, b);
+            }
         case 'x':
             return multiply(a, b);
         case '+':
@@ -71,136 +132,12 @@ function convertOperator(keyboardOperator) {
 }
 
 function useKeyboardInput(e) {
+    if (resultDisplay.textContent === 'ERROR') clearAll();
     if (e.key >= 0 && e.key <=9) appendNumber(e.key);
     if (e.key === '+' || e.key === '-') setOperation(e.key);
     if (e.key === '*' || e.key === '/') setOperation(convertOperator(e.key));
     if (e.key === 'Enter') calculate();
-}
-
-/*
-const numbersBtn = document.querySelectorAll('.numbers');
-const pointBtn = document.querySelector('.point');
-const operatorsBtn = document.querySelectorAll('.operators');
-const equalBtn = document.querySelectorAll('.equal');
-const percentageBtn = document.querySelectorAll('.percentage');
-const clearBtn = document.querySelector('.clear-all');
-const deleteBtn = document.querySelector('.canc');
-const operationDisplay = document.querySelector('.operation');
-const currentValueDisplay = document.querySelector('.current-value');
-
-let val = '';
-let currentOperator = null;
-let n1 = '';
-let n2 = '';
-let cleanOperationDisplay = false;
-
-numbersBtn.forEach(btn => {
-    btn.addEventListener('click', () => appendNumber(btn.textContent));
-});
-pointBtn.addEventListener('click', appendPoint);
-operatorsBtn.forEach(btn => {
-    btn.addEventListener('click', () => getOperation(btn.textContent));
-});
-equalBtn.forEach(btn => {
-    btn.addEventListener('click', execute);
-});
-percentageBtn.forEach(btn => {
-    btn.addEventListener('click', calculatePercent);
-});
-clearBtn.addEventListener('click', clearEverything);
-deleteBtn.addEventListener('click', deleteDigit);
-
-
-function appendNumber(num) {
-    if (cleanOperationDisplay) resetOperationDisplay();
-    currentValueDisplay.textContent += num;
-}
-
-function appendPoint() {
-    if (cleanOperationDisplay) resetOperationDisplay();
-    if (currentValueDisplay.textContent === '') currentValueDisplay.textContent = '0';;
-    if (currentValueDisplay.textContent.includes('.')) 
-        currentValueDisplay.textContent += '.';
-
-}
-//??
-function getOperation(operator) {
-    n1 = Number(val);
-    val = '';
-    if (currentOperator !== '') {
-        n1 += val;
-        execute();
-    }
-    currentOperator = operator;
-    operationDisplay.textContent = `${n1} ${operator}`;
-}
-//??
-function execute() {
-    n2 = currentValueDisplay.textContent;
-    operationDisplay.textContent += ` ${n2} =`;
-    operate(currentOperator, n1, n2);
-}
-//??
-function calculatePercent() {
-    n2 = (n) / 100;
-    currentValueDisplay.textContent = n2;
-}
-
-function resetOperationDisplay() {
-    operationDisplay.textContent = '';
-    cleanOperationDisplay = false;
-}
-
-function clearEverything() {
-    currentOperator = null;
-    n1 = '';
-    n2 = '';
-    console.clear();
-    currentValueDisplay.textContent = '';
-    resetOperationDisplay();
-}
-
-function deleteDigit() {
-    currentValueDisplay.textContent = currentValueDisplay.textContent.toString().slice(0, -1);
-}
-
-const add = (a, b) => currentValueDisplay.textContent = a + b;
-const subtract = (a, b) => currentValueDisplay.textContent = a - b;
-const multiply = (a, b) => currentValueDisplay.textContent = a * b;
-const divide = (a, b) => currentValueDisplay.textContent = a / b;
-
-function operate(operator, a, b) {
-    a = Number(a);
-    b = Number(b);
-    switch (operator) {
-        case ':':
-            if (b == 0) {
-                currentValueDisplay.textContent = 'ERROR';
-            } else {
-                return divide(a, b);
-            }
-        case 'x':
-            return multiply(a, b);
-        case '+':
-            return add(a, b);
-        case '-':
-            return subtract(a, b);
-    }
-}
-
-// Keyboard input
-window.addEventListener('keydown', useKeyboard);
-
-function useKeyboard(e) {
-    if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
-    if (e.key === 'Backspace') deleteDigit();
+    if (e.key === 'Escape') clearAll();
     if (e.key === '.') appendPoint();
-    if (e.key === 'Escape') clearEverything();
-    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') {
-        if (e.key === '*') e.key = 'x';
-        if (e.key === '/') e.key = ':';
-        getOperation(e.key);
-    }
-    if (e.key === 'Enter') execute();
+    if (e.key === 'Backspace') deleteDigit();
 }
-*/
